bd3723cbbb07bc2847a297705bca362ef2b01d29
Ayush Arya <69747175+Ayush01arya@users.noreply.github.com>
2022-02-01 11:47:43 +0530

    Add files via upload

---
+++ stream.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from PIL import Image
+
+st.title("Youtube Tag Genrator")
+a=st.text_input("Enter You Querry","Type here here..")
+if st.button("Search"):
+    import requests
+
+    query = a
+    parms = {"client": "firefox", "q": query, "hl": "en", }
+    url = 'http://suggestqueries.google.com/complete/search'
+    r = requests.get(url, params=parms)
+    r.status_code
+    # print(r.status)
+
+    res = r.json()[1]
+    for data in res:
+
+        st.write(f"{data}",end='')
+
+
+#st.markdown("'<h1 style='text-align:center;color:red;'>some</h1>",unsafe_allow_html=True)
